b0e062cd02f133bdbcc6b66a3d47c2a30d8cf972
Drew Sand <dresand233@gmail.com>
2020-11-03 16:09:54 -0430

    added preloader

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+ import * as preloader from './preoloader.js'
  import * as myWorks from './myWorks.js'
  import * as aboutMe from './aboutMe.js'
  import * as sidebar from './sidebar.js'
  import * as smoothScroll from './smoothScroll.js'
+
+ 
